3f046be69af2e8561dd98fc3edde960b8ae235a2
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-02-24 14:43:25 +0100

    database seed

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Subject;
+use App\Models\UserSubject;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,12 @@ class SubjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $subject=Subject::create([
+            'name' => 'laravel-vue',
+        ]);
+        UserSubject::create([
+            'subject_id' => $subject->id,
+            'user_id' => 1,
+        ]);
     }
 }
