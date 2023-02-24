8f4c104d8eb660b8fe5e45fc37985de0536e1e52
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-02-24 15:08:56 +0100

    vue manuel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+window.Vue = require('vue').default;
+
+import {createApp} from "vue";
+import router from "./router/router";
+import App from "./components/App.vue";
+//import axios from 'axios'
+//import VueAxios from 'vue-axios'  
+//Vue.use(VueAxios, axios)
+//,VueAxios, axios
+// import 'bootstrap/dist/css/bootstrap.css'
+const app = createApp(App);
+app.use(router).mount("#app");
---
+++ resources/js/router/router.js
@@ -0,0 +1,8 @@
+const routes=[
+    {
+        path:'',
+        component: ()=> import('../components/Login.vue')
+    }
+]
+
+export default routes
